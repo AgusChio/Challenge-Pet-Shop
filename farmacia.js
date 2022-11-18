eb87b4ec44b56cce854a75f169bd73ff430386bc
Lu <lucividal09@gmail.com>
2022-11-18 13:57:31 -0300

    filtro por busqueda y precio realizados

--- farmacia.js
+++ farmacia.js
@@ -3,7 +3,10 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            medicamentos: []
+            medicamentos: [],
+            medicamentosFiltrados: [],
+            textoInput: '',
+            seleccionada: ''
         }
     },
     created() {
@@ -17,11 +20,41 @@ const app = createApp({
     methods: {
         obtenerMedicamentos(data) {
             this.medicamentos = data.filter(elemento => elemento.tipo === "Medicamento")
-            console.log(this.medicamentos)
+            this.medicamentos = this.medicamentos.sort((a, b) => a.nombre.localeCompare(b.nombre))
+            this.medicamentosFiltrados = this.medicamentos
+            console.log(this.medicamentosFiltrados)
+        },
+        filtrarMedicamentos() {
+            console.log(this.textoInput)
+            if (this.textoInput) {
+                this.medicamentosFiltrados = this.medicamentos.filter(medicamento => {
+                    return medicamento.nombre.toLowerCase().trim().includes(this.textoInput.toLowerCase().trim())
+                })
+                /* this.medicamentosFiltrados = this.medicamentos.filter(medicamento => medicamento.stock > 5) */
+            } else {
+                this.medicamentosFiltrados = this.medicamentos
+            }
         }
     },
     computed: {
-
+        filtrar() {
+            let ordenarPor;
+            switch (this.seleccionada) {
+                case 'A-Z':
+                    console.log('holaa')
+                    this.medicamentosFiltrados = this.medicamentosFiltrados.sort((a, b) => a.nombre.localeCompare(b.nombre))
+                    break;
+                case 'Mayor precio':
+                    this.medicamentosFiltrados = this.medicamentosFiltrados.sort((a, b) => b.precio - a.precio)
+                    break;
+                case 'Menor precio':
+                    this.medicamentosFiltrados = this.medicamentosFiltrados.sort((a, b) => a.precio - b.precio)
+                    break;
+                default:
+                    this.medicamentosFiltrados = this.medicamentosFiltrados
+            }
+            
+        }
     }
 })
 
